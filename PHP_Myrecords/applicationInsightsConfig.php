9944e0e43a84b75f04579dd5fa99329dcf9d4fd9
blackmetalcoder <peter@blackmetalcoder.se>
2018-01-17 07:57:07 +0100

    Add project files.

---
+++ PHP_Myrecords/applicationInsightsConfig.php
@@ -0,0 +1,12 @@
+<?php 
+/**
+ * ApplicationInsights Configuration
+ *
+ * This is the configuration file for ApplicationInsights.
+ *
+ */
+
+class ApplicationInsightsConfiguration
+{
+    const INSTRUMENTATION_KEY = 'b2231418-3efe-4d97-917d-75063190e5ad';
+}
